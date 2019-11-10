6b410a7ce70589da75f11d6a56d21422378a1db6
sadness_ojisan <sadness.ojisan@gmail.com>
2019-11-11 02:33:01 +0900

    update: babeled

---
+++ example/babeled.js
@@ -0,0 +1,22 @@
+class Hoge {
+  constructor(num, text) {
+    this.validate(num, text);
+  }
+
+  static of(num, text) {
+    new Hoge(num, text);
+    !document.childNodes && (() => {
+      document;
+    })();
+  }
+
+  validate(num, text) {
+    if (typeof num === "number") {
+      console.error("not a number");
+    }
+
+    if (typeof text === "string") {
+      console.error("not a string");
+    }
+  }
+}
